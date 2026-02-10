8cb92123d01c77bf4ab014d612c750c6340cadb3
mryurichiru <mryurichiru@gmail.com>
2026-02-11 07:35:52 +0900

    kadai_005の再提出

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,11 +1,9 @@
-// 変数の宣言
 let myWeight;
+let myHeight;
+let myBmi;
 
-//値の代入
 myWeight = 68;
-
-// 変数の宣言
 myHeight = 1.7;
+myBmi = myWeight / (myHeight * myHeight);
 
-//コンソールへの出力
-console.log(myWeight / (myHeight * myHeight));
+console.log(myBmi);
